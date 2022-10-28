19a0da204ea549b667ef25805c7e028138acc945
Hannah <55860578+plee61@users.noreply.github.com>
2022-10-28 11:46:05 +1100

    Update index.js
    added age input listener

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 document.querySelector('#name').addEventListener('input', function (e) {
     console.log(`name:${e.target.value}`)
 })
+document.querySelector('#age').addEventListener('input', function (e) {
+    console.log(`age:${e.target.value}`)
+})
+
 
 
