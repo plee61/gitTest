789b3d4d89e2feb73191cf57554f34d10132c67d
plee61 <plee61@hotmail.com>
2022-10-28 11:47:05 +1100

    added address listener

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ document.querySelector('#name').addEventListener('input', function (e) {
     console.log(`name:${e.target.value}`)
 })
 
+document.querySelector('#address').addEventListener('input', function (e) {
+    console.log(`address:${e.target.value}`)
+})
 
